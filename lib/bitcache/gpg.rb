b21fd4575fdb42933d1acd651b526e571d021701
Arto Bendiken <arto@bendiken.net>
2008-04-17 16:26:22 +0000

    Implemented a more advanced GnuPG wrapper library.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/bitcache/trunk@40 8c8141c3-5616-4d10-9a90-9230a8688ae7

--- lib/bitcache/gpg.rb
+++ lib/bitcache/gpg.rb
@@ -1,33 +1,72 @@
+require 'open4'
+
 module Bitcache
   module GPG
-    class EncryptionError < RuntimeError ; end
+    class Error < RuntimeError ; end
 
     OPTIONS = {
       :cipher_algo    => :aes256,
       :digest_algo    => :sha1,
       :compress_algo  => :bzip2,
-      :passphrase_fd  => 0,
       :output         => '-',
       :quiet          => true,
       :batch          => true,
       :no_mdc_warning => true, # for decryption
     }
 
-    def self.encrypt(file, password)
-      gpg('-c', escapeshellarg(file)) { |io| io.puts password; io.close_write; io.read }
+    def self.encrypt(data, passphrase, options = {}, &block)
+      output = gpg('-c', options.merge(:passphrase => passphrase)) do |stdin, stdout, stderr|
+        #expect statout, /BEGIN_ENCRYPTION/
+        stdin.write data
+        stdin.close_write
+        #expect statout, /END_ENCRYPTION/
+      end
+      block_given? ? block.call(output) : output.read
     end
 
-    def self.decrypt(file, password)
-      gpg('-d', escapeshellarg(file)) { |io| io.puts password; io.close_write; io.read }
+    def self.decrypt(data, passphrase, options = {}, &block)
+      output = gpg('-d', options.merge(:passphrase => passphrase)) do |stdin, stdout, stderr|
+        #expect statout, /BEGIN_DECRYPTION/
+        stdin.write data
+        stdin.close_write
+        #expect statout, /END_DECRYPTION/
+      end
+      block_given? ? block.call(output) : output.read
     end
 
     protected
 
-      def self.gpg(*args, &block)
-        options = OPTIONS.map { |k, v| !v ? nil : "--#{k.to_s.gsub('_', '-')} #{v == true ? '' : v.to_s}".rstrip }.compact
-        result = IO.popen('gpg ' + (options + args).join(' ') + ' 2>&1', 'r+', &block)
-        raise EncryptionError, result.chomp if $?.exitstatus != 0
-        result
+      def self.expect(io, marker)
+        io.each do |status|
+          puts status if $DEBUG
+          break if status =~ marker
+        end
+      end
+
+      def self.gpg_options(options = {})
+        OPTIONS.merge(options).map { |k, v| !v ? nil : "--#{k.to_s.gsub('_', '-')} #{v == true ? '' : v.to_s}".rstrip }.compact
+      end
+
+      def self.gpg(arg, options = {}, &block)
+        #statout, statin = IO.pipe
+        #options[:status_fd] = statin.fileno
+
+        passphrase = options.delete(:passphrase)
+        passout, passin = IO.pipe
+        options[:passphrase_fd] = passout.fileno
+
+        cmd = 'gpg ' + (gpg_options(options) + [arg]).flatten.join(' ')
+        puts cmd if $DEBUG
+
+        pid, stdin, stdout, stderr = Open4.popen4(cmd)
+        passin.puts passphrase
+        passin.close_write
+
+        block.call(stdin, stdout, stderr)
+        pid, status = Process.waitpid2(pid)
+
+        raise Error, stderr.read.chomp if status.exitstatus != 0
+        stdout
       end
 
       def self.escapeshellarg(arg)
