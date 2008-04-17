6f46292e44a12ec4489e2339dede535235c741aa
Arto Bendiken <arto@bendiken.net>
2008-04-17 17:28:30 +0000

    Refactored GnuPG wrapper for improved flexibility.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/bitcache/trunk@41 8c8141c3-5616-4d10-9a90-9230a8688ae7

--- lib/bitcache/gpg.rb
+++ lib/bitcache/gpg.rb
@@ -2,7 +2,7 @@ require 'open4'
 
 module Bitcache
   module GPG
-    class Error < RuntimeError ; end
+    class Error < IOError ; end
 
     OPTIONS = {
       :cipher_algo    => :aes256,
@@ -11,66 +11,64 @@ module Bitcache
       :output         => '-',
       :quiet          => true,
       :batch          => true,
-      :no_mdc_warning => true, # for decryption
     }
 
-    def self.encrypt(data, passphrase, options = {}, &block)
-      output = gpg('-c', options.merge(:passphrase => passphrase)) do |stdin, stdout, stderr|
-        #expect statout, /BEGIN_ENCRYPTION/
-        stdin.write data
-        stdin.close_write
-        #expect statout, /END_ENCRYPTION/
-      end
+    def self.encrypt(input, passphrase, options = {}, &block)
+      output = gpg('-c', input, options.merge(:passphrase => passphrase))
       block_given? ? block.call(output) : output.read
     end
 
-    def self.decrypt(data, passphrase, options = {}, &block)
-      output = gpg('-d', options.merge(:passphrase => passphrase)) do |stdin, stdout, stderr|
-        #expect statout, /BEGIN_DECRYPTION/
-        stdin.write data
-        stdin.close_write
-        #expect statout, /END_DECRYPTION/
-      end
+    def self.decrypt(input, passphrase, options = {}, &block)
+      output = gpg('-d', input, options.merge(:passphrase => passphrase, :no_mdc_warning => true))
       block_given? ? block.call(output) : output.read
     end
 
     protected
 
-      def self.expect(io, marker)
-        io.each do |status|
-          puts status if $DEBUG
-          break if status =~ marker
+      def self.gpg(command, input, options = {}, &block)
+        case
+          when input.is_a?(Proc)            # input producer block
+            gpg_exec(command, options, &input)
+
+          when input.respond_to?(:realpath) # Pathname
+            gpg_exec("#{command} #{escapeshellarg(input.realpath)}", options)
+
+          when input.respond_to?(:read)     # Stream, IO, File
+            gpg_exec(command, options) { |stdin| stdin.write input.read } # TODO: 4K chunks
+
+          when input.respond_to?(:to_str)   # String
+            gpg_exec(command, options) { |stdin| stdin.write input.to_str }
+
+          else
+            raise ArgumentError, input
         end
+      end
+
+      def self.gpg_exec(command, options = {}, &block)
+        passphrase = options.delete(:passphrase)
+        passout, passin = IO.pipe
+        options[:passphrase_fd] = passout.fileno
+
+        command = 'gpg ' + (gpg_options(options) + [command]).flatten.join(' ')
+        pid, stdin, stdout, stderr = Open4.popen4(command)
+
+        passin.puts passphrase
+        passin.close_write
+
+        block.call(stdin) if block_given?
+        stdin.close_write
+
+        pid, status = Process.waitpid2(pid)
+        raise Error, stderr.read.chomp if status.exitstatus != 0
+        stdout
       end
 
       def self.gpg_options(options = {})
         OPTIONS.merge(options).map { |k, v| !v ? nil : "--#{k.to_s.gsub('_', '-')} #{v == true ? '' : v.to_s}".rstrip }.compact
       end
 
-      def self.gpg(arg, options = {}, &block)
-        #statout, statin = IO.pipe
-        #options[:status_fd] = statin.fileno
-
-        passphrase = options.delete(:passphrase)
-        passout, passin = IO.pipe
-        options[:passphrase_fd] = passout.fileno
-
-        cmd = 'gpg ' + (gpg_options(options) + [arg]).flatten.join(' ')
-        puts cmd if $DEBUG
-
-        pid, stdin, stdout, stderr = Open4.popen4(cmd)
-        passin.puts passphrase
-        passin.close_write
-
-        block.call(stdin, stdout, stderr)
-        pid, status = Process.waitpid2(pid)
-
-        raise Error, stderr.read.chomp if status.exitstatus != 0
-        stdout
-      end
-
       def self.escapeshellarg(arg)
-        return RUBY_PLATFORM =~ /mswin32/ ? "\"#{arg.gsub('"', '\\"')}\"" : "'#{arg.gsub("'", "\\'")}'"
+        return RUBY_PLATFORM =~ /mswin32/ ? "\"#{arg.to_s.gsub('"', '\\"')}\"" : "'#{arg.to_s.gsub("'", "\\'")}'"
       end
   end
 end
