b78136c1dc19bf9a7035e552342d7a95f52774c7
Arto Bendiken <arto@bendiken.net>
2010-01-02 05:18:48 +0100

    Disabled untested features for now.

--- lib/bitcache.rb
+++ lib/bitcache.rb
@@ -1,12 +1,6 @@
 require 'digest/sha1'
-require 'bitcache/utils'
-require 'bitcache/api'
+require 'bitcache/version'
 
 module Bitcache
-  autoload :Adapter,    'bitcache/adapter'
-  autoload :Config,     'bitcache/config'
-  autoload :Encoders,   'bitcache/encoders' # FIXME
-  autoload :Repository, 'bitcache/repository'
-  autoload :Stream,     'bitcache/stream'
-  autoload :VERSION,    'bitcache/version'
+  # TODO
 end
