e4f5557f198fc05d97a3395819f8081384d8169f
Arto Bendiken <arto@bendiken.net>
2010-01-02 05:21:45 +0100

    Bumped version to 0.0.0.

--- lib/bitcache/version.rb
+++ lib/bitcache/version.rb
@@ -2,13 +2,18 @@ module Bitcache
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 1
+    TINY  = 0
     EXTRA = nil
 
     STRING = [MAJOR, MINOR, TINY].join('.')
     STRING << "-#{EXTRA}" if EXTRA
 
+    ##
+    # @return [String]
     def self.to_s()   STRING end
+
+    ##
+    # @return [String]
     def self.to_str() STRING end
   end
 end
