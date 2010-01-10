5dc448b27caee3b669abc153a150b626cdda83ce
Arto Bendiken <arto@bendiken.net>
2010-01-10 20:10:22 +0100

    Implemented Bitcache::Repository#initialize.

--- lib/bitcache/repository.rb
+++ lib/bitcache/repository.rb
@@ -2,5 +2,26 @@ module Bitcache
   ##
   class Repository
     include Enumerable
+
+    ##
+    # @return [Hash{Symbol => Object}]
+    attr_accessor :options
+
+    ##
+    # Initializes this repository instance.
+    #
+    # @param  [Hash{Symbol => Object}] options
+    # @yield  [repository]
+    # @yieldparam [Repository] repository
+    def initialize(options = {}, &block)
+      @options = options
+
+      if block_given?
+        case block.arity
+          when 1 then block.call(self)
+          else instance_eval(&block)
+        end
+      end
+    end
   end
 end
