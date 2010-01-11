1abb4da54a4f413e73df2ade8ba7aa40fe8830bf
Arto Bendiken <arto@bendiken.net>
2010-01-11 05:15:32 +0100

    Added some custom RSpec matchers.

--- lib/bitcache/spec.rb
+++ lib/bitcache/spec.rb
@@ -21,6 +21,36 @@ module Bitcache
           ::Spec::Matchers::Matcher.new(name, *expected, &declarations)
         end
       end
+
+      define :be_an_adapter do
+        match do |value|
+          value.kind_of?(Bitcache::Adapter)
+        end
+      end
+
+      define :be_a_repository do
+        match do |value|
+          value.kind_of?(Bitcache::Repository)
+        end
+      end
+
+      define :be_a_bitstream do
+        match do |value|
+          value.kind_of?(Bitcache::Stream) || value.is_a?(String) # FIXME
+        end
+      end
+
+      define :have_id do |stream|
+        match do |repository|
+          repository.has_id?(stream)
+        end
+      end
+
+      define :have_bitstream do |stream|
+        match do |repository|
+          repository.has_stream?(stream)
+        end
+      end
     end # module Matchers
   end # module Spec
 end # module Bitcache
