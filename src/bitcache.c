8a68f7a2f58b319baa536d48910b77eb67649331
Arto Bendiken <arto@bendiken.net>
2010-12-04 16:30:42 +0100

    Optimized digest computation for empty byte strings by defining canonical precomputed digests.

--- src/bitcache.c
+++ src/bitcache.c
@@ -15,24 +15,69 @@
 
 const char* const bitcache_version_string = PACKAGE_VERSION;
 
+const bitcache_id_md5 const bitcache_md5_empty = {
+  .type   = BITCACHE_MD5,
+  .digest = { 0xd4, 0x1d, 0x8c, 0xd9, 0x8f, 0x00, 0xb2, 0x04,
+              0xe9, 0x80, 0x09, 0x98, 0xec, 0xf8, 0x42, 0x7e },
+  // d41d8cd98f00b204e9800998ecf8427e
+};
+
+const bitcache_id_sha1 const bitcache_sha1_empty = {
+  .type   = BITCACHE_SHA1,
+  .digest = { 0xda, 0x39, 0xa3, 0xee, 0x5e, 0x6b, 0x4b, 0x0d,
+              0x32, 0x55, 0xbf, 0xef, 0x95, 0x60, 0x18, 0x90,
+              0xaf, 0xd8, 0x07, 0x09 },
+  // da39a3ee5e6b4b0d3255bfef95601890afd80709
+};
+
+const bitcache_id_sha256 const bitcache_sha256_empty = {
+  .type   = BITCACHE_SHA256,
+  .digest = { 0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
+              0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
+              0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+              0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55 },
+  // e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+};
+
 //////////////////////////////////////////////////////////////////////////////
 // Digest API
 
 byte*
 bitcache_md5(const byte* data, const size_t size, byte* buffer) {
   assert(data != NULL || size == 0);
+  if (size == 0) {
+    if (buffer != NULL) {
+      memcpy(buffer, bitcache_md5_empty.digest, BITCACHE_MD5_SIZE);
+      return buffer;
+    }
+    return (byte*)bitcache_md5_empty.digest;
+  }
   return MD5(data, size, buffer); // currently uses OpenSSL
 }
 
 byte*
 bitcache_sha1(const byte* data, const size_t size, byte* buffer) {
   assert(data != NULL || size == 0);
+  if (size == 0) {
+    if (buffer != NULL) {
+      memcpy(buffer, bitcache_sha1_empty.digest, BITCACHE_SHA1_SIZE);
+      return buffer;
+    }
+    return (byte*)bitcache_sha1_empty.digest;
+  }
   return SHA1(data, size, buffer); // currently uses OpenSSL
 }
 
 byte*
 bitcache_sha256(const byte* data, const size_t size, byte* buffer) {
   assert(data != NULL || size == 0);
+  if (size == 0) {
+    if (buffer != NULL) {
+      memcpy(buffer, bitcache_sha256_empty.digest, BITCACHE_SHA256_SIZE);
+      return buffer;
+    }
+    return (byte*)bitcache_sha256_empty.digest;
+  }
   return (buffer = NULL); // TODO
 }
 
