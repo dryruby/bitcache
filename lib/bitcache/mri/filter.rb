7dd436ce8cd3f33d880e4a9a57651ed47b3498bc
Arto Bendiken <arto@bendiken.net>
2010-12-07 04:47:51 +0100

    Implemented the Filter#initialize and #initialize_copy methods.

--- lib/bitcache/mri/filter.rb
+++ lib/bitcache/mri/filter.rb
@@ -1,8 +1,61 @@
 module Bitcache
   ##
-  # A Bitcache filter.
+  # A Bloom filter for Bitcache identifiers.
+  #
+  # @see http://en.wikipedia.org/wiki/Bloom_filter
   class Filter < Struct
-    # TODO
+    DEFAULT_CAPACITY = 1024 # elements
+    BITS_PER_ELEMENT = 8    # bits
+
+    ##
+    # Initializes a new filter from the given `bitmap`.
+    #
+    # @example Constructing a new filter
+    #   Filter.new
+    #
+    # @param  [String, Integer] bitmap
+    #   the initial bitmap for the filter
+    # @yield  [filter]
+    # @yieldparam  [Filter] `self`
+    # @yieldreturn [void] ignored
+    def initialize(bitmap = nil, &block)
+      @bitmap = case bitmap
+        when nil     then "\0" * DEFAULT_CAPACITY
+        when Integer then "\0" * bitmap
+        when String  then bitmap.dup
+        else raise ArgumentError, "expected a String or Integer, but got #{bitmap.inspect}"
+      end
+      @bitmap.force_encoding(Encoding::BINARY) if @bitmap.respond_to?(:force_encoding) # for Ruby 1.9+
+
+      if block_given?
+        case block.arity
+          when 0 then instance_eval(&block)
+          else block.call(self)
+        end
+      end
+    end
+
+    ##
+    # Initializes a filter cloned from `original`.
+    #
+    # @param  [Filter] original
+    # @return [void]
+    def initialize_copy(original)
+      @bitmap = original.instance_variable_get(:@bitmap).clone
+    end
+
+    ##
+    # Prevents further modifications to this filter.
+    #
+    # @return [void] `self`
+    def freeze
+      bitmap.freeze
+      super
+    end
+
+    ##
+    # @private
+    attr_reader :bitmap
 
     # Load optimized method implementations when available:
     send(:include, Bitcache::FFI::Filter) if defined?(Bitcache::FFI::Filter)
--- lib/bitcache/spec/filter.rb
+++ lib/bitcache/spec/filter.rb
@@ -7,4 +7,62 @@ share_as :Bitcache_Filter do
     raise '+@class+ must be defined in a before(:all) block' unless instance_variable_get(:@class)
     @filter = @class.new
   end
+
+  describe "Filter.for(enum)" do
+    it "returns a Filter" do
+      # TODO
+    end
+  end
+
+  describe "Filter.new" do
+    it "returns a Filter" do
+      @class.new.should be_a Filter
+    end
+  end
+
+  describe "Filter.new(Integer)" do
+    it "returns a Filter" do
+      @class.new(1024).should be_a Filter
+    end
+  end
+
+  describe "Filter.new(String)" do
+    it "returns a Filter" do
+      @class.new("\0" * 128).should be_a Filter
+    end
+  end
+
+  describe "Filter#clone" do
+    it "returns a Filter" do
+      @filter.clone.should be_a Filter
+    end
+
+    it "returns an identical copy of the filter" do
+      @filter.clone.bitmap.should_not equal @filter.bitmap
+      @filter.clone.bitmap.should eql @filter.bitmap
+    end
+  end
+
+  describe "Filter#dup" do
+    it "returns a Filter" do
+      @filter.dup.should be_a Filter
+    end
+
+    it "returns an identical copy of the filter" do
+      @filter.dup.bitmap.should_not equal @filter.bitmap
+      @filter.dup.bitmap.should eql @filter.bitmap
+    end
+  end
+
+  describe "Filter#freeze" do
+    it "freezes the filter" do
+      @filter.should_not be_frozen
+      @filter.freeze
+      @filter.should be_frozen
+    end
+
+    it "returns self" do
+      @filter.freeze.should equal @filter
+    end
+  end
 end
