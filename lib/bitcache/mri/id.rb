6d509fcba1c9a425797de35de34016faae18e46c
Arto Bendiken <arto@bendiken.net>
2010-12-08 01:11:31 +0100

    Optimized the Identifier#to_s(2) implementation.

--- lib/bitcache/mri/id.rb
+++ lib/bitcache/mri/id.rb
@@ -311,7 +311,7 @@ module Bitcache
         when 16 then digest.unpack('H*').first
         when 10 then to_i.to_s(10).ljust((size * Math.log10(256)).ceil, '0')
         when 8  then to_i.to_s(8).ljust((size * (Math.log(256) / Math.log(8))).ceil, '0')
-        when 2  then to_i.to_s(2).ljust(size * 8, '0') # TODO: optimize
+        when 2  then digest.unpack('B*').first
         else raise ArgumentError, "invalid radix #{base}"
       end
     end
