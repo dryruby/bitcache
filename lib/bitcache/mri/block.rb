d409285b61784b42934580f7f37eba1c8e83ac0d
Arto Bendiken <arto@bendiken.net>
2010-12-13 01:01:35 +0100

    Defined the Block#id, #size, and #data accessors.

--- lib/bitcache/mri/block.rb
+++ lib/bitcache/mri/block.rb
@@ -2,7 +2,37 @@ module Bitcache
   ##
   # A Bitcache data block.
   class Block < Struct
-    # TODO
+    ##
+    # Initializes a new block.
+    def initialize(&block)
+      @size, @data = 0, StringIO.new
+
+      if block_given?
+        case block.arity
+          when 0 then instance_eval(&block)
+          else block.call(self)
+        end
+      end
+    end
+
+    ##
+    # The block identifier.
+    #
+    # @return [Identifier]
+    attr_reader :id
+
+    ##
+    # The block size.
+    #
+    # @return [Integer]
+    #   a non-negative integer in the range `(0...(2**64))`
+    attr_reader :size
+
+    ##
+    # The block data stream.
+    #
+    # @return [IO]
+    attr_reader :data
 
     # Load accelerated method implementations when available:
     send(:include, Bitcache::FFI::Block) if defined?(Bitcache::FFI::Block)
--- lib/bitcache/spec/block.rb
+++ lib/bitcache/spec/block.rb
@@ -2,4 +2,40 @@ require 'bitcache/spec'
 
 share_as :Bitcache_Block do
   include Bitcache::Spec::Matchers
+
+  before :each do
+    raise '+@class+ must be defined in a before(:all) block' unless instance_variable_get(:@class)
+    @block = @class.new
+  end
+
+  describe "Block.new" do
+    it "returns a new Block" do
+      @class.new.should be_a @class
+    end
+
+    it "yields self if passed a block" do
+      yielded = nil
+      block = @class.new { |block| yielded = block }
+      yielded.should be_a Block
+      yielded.should equal block
+    end
+  end
+
+  describe "Block#id" do
+    it "returns an Identifier" do
+      @block.id.should be_an Identifier
+    end
+  end
+
+  describe "Block#size" do
+    it "returns an Integer" do
+      @block.size.should be_an Integer
+    end
+  end
+
+  describe "Block#data" do
+    it "returns an IO stream" do
+      [IO, StringIO].should include @block.data.class
+    end
+  end
 end
