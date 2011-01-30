baf99f88efbec884295c303468411c35c5f7a083
Arto Bendiken <arto@bendiken.net>
2011-01-31 00:25:48 +0100

    Improved POSIX signal handling in the ZeroMQ event loop.

--- lib/bitcache/zeromq/loop.rb
+++ lib/bitcache/zeromq/loop.rb
@@ -74,26 +74,66 @@ module Bitcache::ZeroMQ
     # @return [void] `self`
     def setup
       Signal.trap(:TERM, method(:on_sigterm))
-      Signal.trap(:INT, method(:on_sigint))
-    end
-
-    ##
-    # Handles the `SIGINT` signal.
-    #
-    # @return [void]
-    # @see    http://en.wikipedia.org/wiki/SIGINT_(POSIX)
-    def on_sigint(*args)
-      die
+      Signal.trap(:INT,  method(:on_sigint))
+      Signal.trap(:HUP,  method(:on_sighup))
+      Signal.trap(:USR1, method(:on_sigusr1))
+      Signal.trap(:USR2, method(:on_sigusr2))
     end
 
     ##
     # Handles the `SIGTERM` signal.
     #
+    # @param  [Integer] signum
     # @return [void]
     # @see    http://en.wikipedia.org/wiki/SIGTERM
     # @see    http://en.wikipedia.org/wiki/SIGKILL
-    def on_sigterm(*args)
+    def on_sigterm(signum)
+      log.info("Received a SIGTERM (#{signum}) signal, terminating...") if respond_to?(:log)
       die
+    end
+
+    ##
+    # Handles the `SIGINT` signal.
+    #
+    # @param  [Integer] signum
+    # @return [void]
+    # @see    http://en.wikipedia.org/wiki/SIGINT_(POSIX)
+    def on_sigint(signum)
+      log.info("Received a SIGINT (#{signum}) signal, terminating...") if respond_to?(:log)
+      die
+    end
+
+    ##
+    # Handles the `SIGHUP` signal.
+    #
+    # @param  [Integer] signum
+    # @return [void]
+    # @see    http://en.wikipedia.org/wiki/SIGHUP
+    def on_sighup(signum)
+      log.info("Received a SIGHUP (#{signum}) signal, terminating...") if respond_to?(:log)
+      die
+    end
+
+    ##
+    # Handles the `SIGUSR1` signal.
+    #
+    # @param  [Integer] signum
+    # @return [void]
+    # @see    http://en.wikipedia.org/wiki/SIGUSR1_and_SIGUSR2
+    def on_sigusr1(signum)
+      log.info("Received a SIGUSR1 (#{signum}) signal.") if respond_to?(:log)
+      # no-op by default
+    end
+
+    ##
+    # Handles the `SIGUSR2` signal.
+    #
+    # @param  [Integer] signum
+    # @return [void]
+    # @see    http://en.wikipedia.org/wiki/SIGUSR1_and_SIGUSR2
+    def on_sigusr2(signum)
+      log.info("Received a SIGUSR2 (#{signum}) signal.") if respond_to?(:log)
+      # no-op by default
     end
 
   protected
