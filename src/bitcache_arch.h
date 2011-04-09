5a5249dfb0895aa8040ce6312be65cf41c75b052
Arto Bendiken <arto@bendiken.net>
2011-04-09 10:59:02 +0200

    Defined some GCC-specific function attribute macros.

--- src/bitcache_arch.h
+++ src/bitcache_arch.h
@@ -7,14 +7,37 @@
 extern "C" {
 #endif
 
-/* branch prediction hints */
-#if 1
-#define likely(x)         __builtin_expect(!!(x), 1) /* `x` is likely to evaluate to TRUE   */
-#define unlikely(x)       __builtin_expect(!!(x), 0) /* `x` is unlikely to evaluate to TRUE */
-#else
-#define likely(x)         x
-#define unlikely(x)       x
+#ifndef GCC_VERSION
+# define GCC_VERSION (__GNUC__ * 1000 + __GNUC_MINOR__)
 #endif
+
+/* GCC-specific optimizations */
+// @see http://gcc.gnu.org/onlinedocs/gcc/Function-Attributes.html
+#ifdef __GNUC__
+# define FLATTEN __attribute__((__flatten__)  /* inline every call inside the function, if possible */
+# define PURE    __attribute__((__pure__))    /* declare that the function has no side effects */
+# if GCC_VERSION >= 4003
+#  define HOT    __attribute__((__hot__))     /* the function is a hot spot (GCC 4.3+ only) */
+#  define COLD   __attribute__((__cold__))    /* the function is unlikely to be executed (GCC 4.3+ only) */
+# else
+#  define HOT
+#  define COLD
+# endif
+#else  /* !__GNUC__ */
+# define FLATTEN
+# define PURE
+# define HOT
+# define COLD
+#endif /* __GNUC__ */
+
+/* branch prediction hints */
+#ifdef __GNUC__
+# define likely(x)         __builtin_expect(!!(x), 1) /* `x` is likely to evaluate to TRUE   */
+# define unlikely(x)       __builtin_expect(!!(x), 0) /* `x` is unlikely to evaluate to TRUE */
+#else
+# define likely(x)         x
+# define unlikely(x)       x
+#endif /* __GNUC__ */
 
 #ifdef __cplusplus
 }
