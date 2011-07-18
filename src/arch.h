a2c76549b32a03e8c5820bd4ddb8c35378526fcd
Arto Bendiken <arto@bendiken.net>
2011-07-18 13:49:30 +0200

    Simplified GCC-version-specific macro definitions using __GNUC_VERSION__.

--- src/arch.h
+++ src/arch.h
@@ -7,17 +7,13 @@
 extern "C" {
 #endif
 
-#ifndef GCC_VERSION
-# define GCC_VERSION (__GNUC__ * 1000 + __GNUC_MINOR__)
-#endif
-
 /* GCC-specific optimizations */
 // @see http://gcc.gnu.org/onlinedocs/gcc/Function-Attributes.html
 #ifdef __GNUC__
 # define NONNULL __attribute__((__nonnull__)) /* the function requires non-NULL arguments */
 # define FLATTEN __attribute__((__flatten__)  /* inline every call inside the function, if possible */
 # define PURE    __attribute__((__pure__))    /* declare that the function has no side effects */
-# if GCC_VERSION >= 4003
+# if __GNUC_VERSION__ >= 40300
 #  define HOT    __attribute__((__hot__))     /* the function is a hot spot (GCC 4.3+ only) */
 #  define COLD   __attribute__((__cold__))    /* the function is unlikely to be executed (GCC 4.3+ only) */
 # else
