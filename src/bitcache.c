344bc13d904e89f1d07948f4757c72d093d17754
Arto Bendiken <arto@bendiken.net>
2011-07-18 15:52:59 +0200

    Defined the bitcache_feature_names and bitcache_feature_count constants that can be used to determine with which features libbitcache was compiled.

--- src/bitcache.c
+++ src/bitcache.c
@@ -3,3 +3,16 @@
 #include "build.h"
 
 const char* const bitcache_version_string = PACKAGE_VERSION;
+
+const char* const bitcache_feature_names[] = {
+  "base",
+#ifndef NDEBUG
+  "debug",
+#endif
+#ifndef DISABLE_THREADS
+  "threads",
+#endif
+};
+
+const unsigned int bitcache_feature_count =
+  sizeof(bitcache_feature_names) / sizeof(bitcache_feature_names[0]);
