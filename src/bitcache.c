b2461e8b9ff14999fe794b27145781ec4a898786
Arto Bendiken <arto@bendiken.net>
2011-08-28 06:16:21 +0200

    Implemented the bitcache_has_feature() and bitcache_has_module() functions.

--- src/bitcache.c
+++ src/bitcache.c
@@ -35,3 +35,27 @@ const char* const bitcache_module_names[] = {
 
 const unsigned int bitcache_module_count =
   (sizeof(bitcache_module_names) / sizeof(bitcache_module_names[0])) - 1;
+
+bool
+bitcache_has_feature(const char* const restrict name) {
+  validate_with_false_return(name != NULL);
+
+  for (unsigned int i = 0; i < bitcache_feature_count; i++) {
+    if (unlikely(str_equal(bitcache_feature_names[i], name))) {
+      return TRUE;
+    }
+  }
+  return FALSE;
+}
+
+bool
+bitcache_has_module(const char* const restrict name) {
+  validate_with_false_return(name != NULL);
+
+  for (unsigned int i = 0; i < bitcache_module_count; i++) {
+    if (unlikely(str_equal(bitcache_module_names[i], name))) {
+      return TRUE;
+    }
+  }
+  return FALSE;
+}
